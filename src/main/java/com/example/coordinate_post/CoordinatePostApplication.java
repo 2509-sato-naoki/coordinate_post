54e4afe7d904523add23a5f322e9ca01c2053f91
sato_naoki <sato.naoki@alhinc.jp>
2025-12-24 11:57:09 +0900

    firstcommit

---
+++ src/main/java/com/example/coordinate_post/CoordinatePostApplication.java
@@ -0,0 +1,13 @@
+package com.example.coordinate_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinatePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinatePostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coordinate_post/CoordinatePostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coordinate_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatePostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
